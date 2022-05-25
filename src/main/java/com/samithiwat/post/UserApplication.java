aaf91af5173a138a39da709d0ae6302c8d09f005
samithiwat <samithiwat2544@gmail.com>
2022-05-25 19:18:06 +0700

    Rename package to post

--- src/main/java/com/samithiwat/post/UserApplication.java
+++ src/main/java/com/samithiwat/post/UserApplication.java
@@ -1,4 +1,4 @@
-package com.samithiwat.user;
+package com.samithiwat.post;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/test/java/com/samithiwat/post/UserApplicationTests.java
@@ -0,0 +1,19 @@
+package com.samithiwat.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringBootTest(properties = {
+		"grpc.server.inProcessName=test-user-application", // Enable inProcess server
+		"grpc.server.port=-1", // Disable external server
+		"grpc.client.userApplication.address=in-process:test" // Configure the client to connect to the inProcess server
+})
+@SpringJUnitConfig(classes = {TestConfig.class})
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
