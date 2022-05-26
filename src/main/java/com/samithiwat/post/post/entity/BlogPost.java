42e32282f2c66d40a74ee4e3e81443dc6010cb23
samithiwat <samithiwat2544@gmail.com>
2022-05-26 22:36:04 +0700

    Update BlogPost entity

--- src/main/java/com/samithiwat/post/post/entity/BlogPost.java
+++ src/main/java/com/samithiwat/post/post/entity/BlogPost.java
@@ -1,5 +1,6 @@
 package com.samithiwat.post.post.entity;
 
+import com.samithiwat.post.bloguser.entity.BlogUser;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -9,18 +10,29 @@ import javax.persistence.*;
 import java.time.Instant;
 
 @Entity
-@Table(name = "post")
+@Table(name = "post", indexes = @Index(columnList = "slug"))
 @SQLDelete(sql = "UPDATE user SET deletedDate = CURRENT_DATE WHERE id = ?")
 @Where(clause = "deletedDate IS NULL")
 public class BlogPost {
+    public BlogPost() {}
+
+    public BlogPost(BlogUser author, String slug, String summary, Boolean isPublished, Instant publishDate) {
+        this.setAuthor(author);
+        this.setSlug(slug);
+        this.setSummary(summary);
+        this.setPublished(isPublished);
+        this.setPublishDate(publishDate);
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column
-    private Long authorId;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private BlogUser author;
 
-    @Column
+    @Column(unique = true)
     private String slug;
 
     @Column
@@ -40,4 +52,76 @@ public class BlogPost {
 
     @Column
     private Instant deletedDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BlogUser getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(BlogUser author) {
+        this.author = author;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public Boolean getPublished() {
+        return isPublished;
+    }
+
+    public void setPublished(Boolean published) {
+        isPublished = published;
+    }
+
+    public Instant getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(Instant publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public Instant getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Instant createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Instant getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Instant updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    public Instant getDeletedDate() {
+        return deletedDate;
+    }
+
+    public void setDeletedDate(Instant deletedDate) {
+        this.deletedDate = deletedDate;
+    }
 }
