1e8eb24eb49d0e57d0da7b482da5299ab0f1550b
samithiwat <samithiwat2544@gmail.com>
2022-05-27 19:07:52 +0700

    Add update method in repository interface

--- src/main/java/com/samithiwat/post/post/BlogPostRepository.java
+++ src/main/java/com/samithiwat/post/post/BlogPostRepository.java
@@ -1,15 +1,21 @@
 package com.samithiwat.post.post;
 
 import com.samithiwat.post.post.entity.BlogPost;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
 import java.util.Optional;
 
 @Repository
 public interface BlogPostRepository extends CrudRepository<BlogPost, Long> {
     @Query("SELECT p FROM BlogPost p WHERE p.slug = :slug")
     Optional<BlogPost> findBySlug(@Param("slug") String slug);
+
+    @Modifying
+    @Query("UPDATE BlogPost p SET p.slug = :slug, p.summary = :summary, p.isPublished = :isPublished, p.publishDate = :publishDate WHERE p.id = :id")
+    boolean update(@Param("id") int id, @Param("slug") String slug, @Param("summary") String summary, @Param("isPublish") boolean isPublish, @Param("publishDate") Instant publishDate);
 }
