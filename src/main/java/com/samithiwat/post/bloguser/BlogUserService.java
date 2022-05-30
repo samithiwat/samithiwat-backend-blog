4d9a332a7059b100b37f2e35a13d5add397790b3
samithiwat <samithiwat2544@gmail.com>
2022-05-31 00:35:59 +0700

    Change method findByUserId to findOneOrCreate

--- src/main/java/com/samithiwat/post/bloguser/BlogUserService.java
+++ src/main/java/com/samithiwat/post/bloguser/BlogUserService.java
@@ -4,5 +4,5 @@ import com.samithiwat.post.grpc.dto.BlogUser;
 
 public interface BlogUserService {
     BlogUser findOne(Long id);
-    com.samithiwat.post.bloguser.entity.BlogUser findOneEntityByUserId(Long id);
+    com.samithiwat.post.bloguser.entity.BlogUser findOneOrCreate(Long userId);
 }
