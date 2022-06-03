acccead7ed85a379e8309d0473a8f3c19ed6b49f
samithiwat <samithiwat2544@gmail.com>
2022-06-03 15:07:29 +0700

    Remove unused statement

--- src/main/java/com/samithiwat/post/comment/BlogCommentRepository.java
+++ src/main/java/com/samithiwat/post/comment/BlogCommentRepository.java
@@ -2,7 +2,6 @@ package com.samithiwat.post.comment;
 
 import com.samithiwat.post.comment.entity.Comment;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface BlogCommentRepository extends CrudRepository<Comment,Long>, PagingAndSortingRepository<Comment, Long> {
+public interface BlogCommentRepository extends CrudRepository<Comment,Long>{
 }
