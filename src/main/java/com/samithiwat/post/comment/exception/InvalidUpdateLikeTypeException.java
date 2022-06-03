b89eeab03b38269324e53df1aabf779a02b73532
samithiwat <samithiwat2544@gmail.com>
2022-06-03 16:36:13 +0700

    Create Invalid type exception

---
+++ src/main/java/com/samithiwat/post/comment/exception/InvalidUpdateLikeTypeException.java
@@ -0,0 +1,7 @@
+package com.samithiwat.post.comment.exception;
+
+public class InvalidUpdateLikeTypeException extends Exception{
+    public InvalidUpdateLikeTypeException(){
+        super("Invalid update type");
+    }
+}
