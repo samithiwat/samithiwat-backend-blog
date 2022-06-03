f80adcf26f8c51eb6ad8d2ba3d87e0cb8570c85b
samithiwat <samithiwat2544@gmail.com>
2022-06-03 15:07:50 +0700

    Implement findAll method

---
+++ src/main/java/com/samithiwat/post/comment/BlogCommentServiceImpl.java
@@ -0,0 +1,74 @@
+package com.samithiwat.post.comment;
+
+import com.samithiwat.post.comment.entity.Comment;
+import com.samithiwat.post.grpc.blogcomment.*;
+import com.samithiwat.post.grpc.dto.BlogComment;
+import com.samithiwat.post.post.BlogPostServiceImpl;
+import com.samithiwat.post.post.entity.Post;
+import io.grpc.stub.StreamObserver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+public class BlogCommentServiceImpl extends BlogCommentServiceGrpc.BlogCommentServiceImplBase {
+    @Autowired
+    BlogCommentRepository repository;
+
+    @Autowired
+    BlogPostServiceImpl blogPostService;
+
+    public BlogCommentServiceImpl(BlogCommentRepository repository, BlogPostServiceImpl blogPostService){
+        this.repository = repository;
+        this.blogPostService = blogPostService;
+    }
+
+    @Override
+    public void findAllCommentFromPost(FindAllCommentByPostRequest request, StreamObserver<BlogCommentListResponse> responseObserver) {
+        BlogCommentListResponse.Builder res = BlogCommentListResponse.newBuilder();
+
+        Post post = this.blogPostService.findOneEntityBySlug(request.getSlug());
+
+        if (post == null){
+            res.setStatusCode(HttpStatus.NOT_FOUND.value())
+                    .addErrors("Not found post");
+
+            responseObserver.onNext(res.build());
+            responseObserver.onCompleted();
+            return;
+        }
+
+        List<Comment> comments = post.getComments();
+
+        for (Comment comment: comments) {
+            BlogComment.Builder result = BlogComment.newBuilder()
+                    .setId(Math.toIntExact(comment.getId()))
+                    .setContent(comment.getContent())
+                    .setCreatedDate(comment.getCreatedDate().toString())
+                    .setUpdatedDate(comment.getUpdatedDate().toString())
+                    .setLikes(Math.toIntExact(comment.getLikes()));
+
+            res.addData(result);
+        }
+
+        res.setStatusCode(HttpStatus.OK.value());
+
+        responseObserver.onNext(res.build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void create(CreateCommentRequest request, StreamObserver<BlogCommentResponse> responseObserver) {
+        super.create(request, responseObserver);
+    }
+
+    @Override
+    public void update(UpdateCommentRequest request, StreamObserver<BlogCommentResponse> responseObserver) {
+        super.update(request, responseObserver);
+    }
+
+    @Override
+    public void delete(DeleteCommentRequest request, StreamObserver<BlogCommentStatusResponse> responseObserver) {
+        super.delete(request, responseObserver);
+    }
+}
