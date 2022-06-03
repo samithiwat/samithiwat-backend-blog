9f379660c85dd9c0015a1795b31ff3ade73b1e3d
samithiwat <samithiwat2544@gmail.com>
2022-06-04 01:55:06 +0700

    Add repository implementation

--- src/main/java/com/samithiwat/post/tag/BlogTagRepository.java
+++ src/main/java/com/samithiwat/post/tag/BlogTagRepository.java
@@ -2,6 +2,7 @@ package com.samithiwat.post.tag;
 
 import com.samithiwat.post.tag.entity.Tag;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface BlogTagRepository extends CrudRepository<Tag, Long> {
+public interface BlogTagRepository extends CrudRepository<Tag, Long>, PagingAndSortingRepository<Tag, Long> {
 }
