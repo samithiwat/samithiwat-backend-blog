a8ff9799ae1a9593c333c886e4d3f68930bd4c15
samithiwat <samithiwat2544@gmail.com>
2022-06-04 16:53:39 +0700

    Fix stub bean problem

--- src/main/java/com/samithiwat/blog/config/GrpcConfig.java
+++ src/main/java/com/samithiwat/blog/config/GrpcConfig.java
@@ -2,14 +2,18 @@ package com.samithiwat.blog.config;
 
 import com.samithiwat.blog.grpc.user.UserServiceGrpc;
 import net.devh.boot.grpc.client.inject.GrpcClient;
-import net.devh.boot.grpc.client.inject.GrpcClientBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@GrpcClientBean(
-        clazz = UserServiceGrpc.UserServiceBlockingStub.class,
-        beanName = "UserServiceBlockingStub",
-        client = @GrpcClient("UserService")
-)
 public class GrpcConfig {
+
+    @GrpcClient("UserService")
+    UserServiceGrpc.UserServiceBlockingStub userClient;
+
+    @Bean
+    UserServiceGrpc.UserServiceBlockingStub userClient(){
+        return userClient;
+    }
+
 }
